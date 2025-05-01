db25fb820841f7fbca7c4b7bf7d9e283611bd4a5
Karasaev Magomed <rostovmen95@gmail.com>
2025-05-01 10:06:14 +0300

    get api

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import './scss/app.scss';
 
 import { Header } from './components/Header';
@@ -5,23 +6,50 @@ import { Categories } from './components/Categories';
 import { Sort } from './components/Sort';
 import { PizzaBlock } from './components/PizzaBlock';
 
-import { pizzaCardMock } from './mocs/pizzaCart.js';
-
-console.log(pizzaCardMock);
-
 export const App = () => {
+  const [pizzasData, setPizzasData] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://681270ec129f6313e20e98c0.mockapi.io/pizzas');
+
+        if (!response.ok) {
+          throw new Error('Failed to fetch pizzas');
+        }
+
+        const pizzaResponse = await response.json();
+        setPizzasData(pizzaResponse);
+        setLoading(false);
+      } catch (error) {
+        setError(error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <div>...loading</div>;
+  }
+
+  if (error) {
+    return <div>...error</div>;
+  }
+
   return (
     <div className='wrapper'>
       <Header />
       <div className='content'>
         <div className='container'>
           <div className='content__top'>
-            {/*<Categories/>*/}
+            <Categories />
             <Sort />
           </div>
           <h2 className='content__title'>Все пиццы</h2>
           <div className='content__items'>
-            {pizzaCardMock.map((obj) => (
+            {pizzasData.map((obj) => (
               <PizzaBlock
                 key={obj.id}
                 image={obj.imageUrl}
